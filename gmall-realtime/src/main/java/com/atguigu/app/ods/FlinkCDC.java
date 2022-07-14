1aef8e8aaedfd04f7c832200115985414a9cd661
dingc <1109928059@qq.com>
2022-07-15 01:05:20 +0800

    FlinkCDC监控MySQL到Kafka

---
+++ gmall-realtime/src/main/java/com/atguigu/app/ods/FlinkCDC.java
@@ -0,0 +1,10 @@
+package com.atguigu.app.ods;
+
+/**
+ * @Author dingc
+ * @Date 2022-07-15 1:02
+ * @Description
+ */
+public class FlinkCDC {
+
+}
---
+++ gmall-realtime/src/main/java/com/atguigu/utils/MyKafkaUtil.java
@@ -0,0 +1,10 @@
+package com.atguigu.utils;
+
+/**
+ * @Author dingc
+ * @Date 2022-07-15 1:03
+ * @Description
+ */
+public class MyKafkaUtil {
+
+}
