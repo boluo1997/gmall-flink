5b277cbc249e5ef234b7a040def1a4f45645ec3b
dingc <1109928059@qq.com>
2022-07-15 09:52:42 +0800

    FlinkSqlCDC

---
+++ gmall-flink-cdc/src/main/java/config/Config.java
@@ -0,0 +1,15 @@
+package config;
+
+public class Config {
+
+    public static final String MYSQL_CDC1_HOSTNAME = "mysql.cdc1.hostname";
+    public static final String MYSQL_CDC1_PORT = "mysql.cdc1.port";
+    public static final String MYSQL_CDC1_USERNAME = "mysql.cdc1.username";
+    public static final String MYSQL_CDC1_PASSWORD = "mysql.cdc1.password";
+    public static final String MYSQL_CDC1_DATABASE = "mysql.cdc1.database";
+
+    public static final String MYSQL_SINK1_URL = "mysql.sink1.url";
+    public static final String MYSQL_SINK1_USERNAME = "mysql.sink1.username";
+    public static final String MYSQL_SINK1_PASSWORD = "mysql.sink1.password";
+
+}
